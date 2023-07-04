d9480372ab62dee3f375015dd49b33aa7cce0d26
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2023-07-04 16:56:06 +0100

    adjust robots.txt

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/robots.txt",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/plain",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
