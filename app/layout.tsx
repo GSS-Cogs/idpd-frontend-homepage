9235cbcd3b4c25b7b4a4cc329e605d61f58c1db9
rossbowen <ross.bowen@ons.gov.uk>
2023-07-11 16:09:56 +0100

    Initial import of govuk-frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  if (process.env.NEXT_MANUAL_SIG_HANDLE) {
+    process.on("SIGTERM", () => {
+      console.log("Received SIGTERM: cleaning up");
+      process.exit(0);
+    });
+
+    process.on("SIGINT", () => {
+      console.log("Received SIGINT: cleaning up");
+      process.exit(0);
+    });
+  }
   return (
     <html lang="en" className="govuk-template">
       <body className="govuk-template__body">
