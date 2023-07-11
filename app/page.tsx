e85a77285ed7ee1ef5d6ffbac46b9097e64b8700
rossbowen <ross.bowen@ons.gov.uk>
2023-07-11 16:09:56 +0100

    Initial import of govuk-frontend

---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+export default async function Home() {
+  return (
+    <div className="govuk-width-container">
+      <main className="govuk-main-wrapper" id="main-content" role="main">
+        <h1 className="govuk-heading-xl">Hello world!</h1>
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
+            <p className="govuk-body">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              dapibus odio sit amet orci efficitur, ut rutrum tortor semper.
+              Praesent vestibulum massa nibh, nec blandit felis posuere a. Donec
+              vestibulum, nisi sit amet sodales malesuada, enim diam maximus
+              lacus, ac sollicitudin ante turpis id dui. Proin tempor aliquam ex
+              eu accumsan. Quisque elementum, nisi at dapibus pellentesque, odio
+              mauris vehicula metus, sit amet tempus metus tellus sed turpis.
+              Nullam at justo maximus, elementum turpis et, aliquam nisl. Nullam
+              eget justo tincidunt, rhoncus felis at, pellentesque nibh. Proin
+              sed ante quam. Nunc sapien est, rhoncus a dui et, tincidunt
+              porttitor metus.
+            </p>
+          </div>
+          <div className="govuk-grid-column-one-third">
+            <p className="govuk-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
+              recusandae! Illum eligendi totam sapiente exercitationem quidem
+              culpa voluptatem, magni labore animi perferendis adipisci quasi
+              tempore molestiae maiores sunt quis voluptatibus.
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
