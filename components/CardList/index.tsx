c33b567ddb1b931ccccf7ba1bbfb60afab729d7c
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2023-10-02 13:19:35 +0100

    mobile view for dataset card

--- components/CardList/index.tsx
+++ components/CardList/index.tsx
@@ -1,7 +1,8 @@
 import { CardProps } from "./types";
+import Image from "next/image";
 
 const CardListTitle = ({ children }: { children: any }) => (
-  <h3 className="govuk-heading-m">{children}</h3>
+  <h3 className="govuk-heading-m app-cards__title">{children}</h3>
 );
 const CardListLink = ({ href, children }: { href: string; children: any }) => (
   <a href={href} className="govuk-link app-cards__link">
@@ -14,7 +15,7 @@ const CardListPublisherCard = ({ items }: { items: CardProps[] }) => {
     <ul className="app-cards__list" data-track-count="cardList">
       {items.map((item: CardProps) => (
         <li className="app-cards__list-item--publisher">
-          <h2 className="app-cards__sub-heading govuk-heading-s">
+          <h2 className="app-cards__sub-heading--publisher govuk-heading-s">
             <a
               className="govuk-link app-cards__link--publisher"
               href={item.href}
@@ -49,22 +50,91 @@ const CardListTopicCard = ({ items }: { items: CardProps[] }) => {
   );
 };
 
-const CardListDatasetCard = ({ items }: { items: CardProps[] }) => {
+function formatDate(date: string) {
+  const d = new Date(date);
+  const options: Intl.DateTimeFormatOptions = {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+  return d
+    .toLocaleDateString("en-GB", options)
+    .replace(",", "")
+    .replace(/( AM| PM)/g, (m) => m.trim().toLowerCase());
+}
+
+const CardListDatasetCard = ({ items }: { items: any }) => {
   return (
     <ul
       className="app-cards__list app-cards__list--one-column"
       data-track-count="cardList"
     >
-      {items.map((item: CardProps) => (
-        <li className="app-cards__list-item--dataset">
-          <h2 className="app-cards__sub-heading--dataset govuk-heading-s">
-            <a className="govuk-link app-cards__link--dataset" href={item.href}>
-              {item.heading}
-            </a>
-          </h2>
-        </li>
+      {items.map((item: any) => (
+        <>
+          <DatasetCardDesktopView item={item} />
+          <DatasetCardMobileView item={item} />
+        </>
       ))}
     </ul>
+  );
+};
+
+const DatasetCardDesktopView = ({ item }: { item: any }) => {
+  return (
+    <li className="app-cards__list-item--dataset">
+      <h2 className="app-cards__sub-heading--dataset govuk-heading-s">
+        <a className="govuk-link app-cards__link--dataset" href={item.creator}>
+          {item?.datasetTitle}
+        </a>
+      </h2>
+    </li>
+  );
+};
+
+const DatasetCardMobileView = ({ item }: { item: any }) => {
+  return (
+    <li className="app-datasets-list__item">
+      <div className="app-datasets-list__item-top">
+        <a
+          className="app-datasets-list__item-title app-datasets-list__item-title--context govuk-link"
+          href={item.creator}
+        >
+          {item?.datasetTitle}
+        </a>
+        <p className="app-datasets-list__item-description">
+          {item?.shortDescription}
+        </p>
+      </div>
+      <div className="app-datasets-list__item-bottom">
+        <div className="app-datasets-list__item-bottom-publisher">
+          <Image
+            src="/assets/images/crest.png"
+            width={40}
+            height={34}
+            alt="Govuk Crest"
+          />
+          <h3 className="app-datasets-list__item-publisher-inner">
+            {item?.publisher}
+          </h3>
+        </div>
+        <ul
+          className="app-datasets-list__item-metadata"
+          style={{ textAlign: "right" }}
+        >
+          <li className="app-datasets-list__item-metadata-row">
+            <div>{item?.topic}</div>
+          </li>
+          <li className="app-datasets-list__item-metadata-row">
+            <time dateTime={item.modified?.value}>{item?.subTopic}</time>
+          </li>
+          <li className="app-datasets-list__item-metadata-row">
+            <time dateTime={item.modified?.value}>
+              Updated: {formatDate(item.modified)}
+            </time>
+          </li>
+        </ul>
+      </div>
+    </li>
   );
 };
 
