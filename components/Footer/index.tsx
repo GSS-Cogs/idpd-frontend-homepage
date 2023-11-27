7dfcbbdb98d1328820f6b29c28d147376afe5023
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2023-11-27 16:08:33 +0000

    styling

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,19 @@
 import { getTopics } from "../../libs/dataRequests";
-import {
-  CardList,
-  CardListLink,
-  CardListFooterTopicCard,
-  CardListTitle,
-} from "@/components/CardList";
+import { CardList, CardListLink, CardListTitle } from "@/components/CardList";
+import topicItems from "../../app/data/topicItems.json";
+
+const popularItems = [
+  { text: "About the data service", href: "" },
+  { text: "IDS Hub", href: "https://integrateddataservice.gov.uk/" },
+  {
+    text: "Climate change portal",
+    href: "https://climate-change.data.gov.uk/",
+  },
+  {
+    text: "Violence against women and girls portal",
+    href: "https://vawg.gss-data.org.uk/",
+  },
+];
 
 export default async function Footer({}) {
   const topics = await getTopics();
@@ -17,71 +26,78 @@ export default async function Footer({}) {
   };
   const parentTopics = getParentTopics(topics.topics);
 
+  const PopularItem = ({ text, href }: { text: string; href: string }) => (
+    <li className="app-footer__popular-item">
+      <a className="govuk-link app-footer__popular-link" href={href}>
+        {text}
+      </a>
+    </li>
+  );
 
   return (
-    <footer className="govuk-footer" role="contentinfo">
-        <div className="app-width-container">
-          <div className="govuk-grid-column-two-thirds">
-              <CardList>
-                  <CardListTitle>Topics</CardListTitle>
-                      {parentTopics.map((link, index) => (
-                  <CardListLink key={index} href={"/"+link.identifier}>
-                        {link.title}
-                </CardListLink>
-          ))}
-                      </CardList>
-          </div>
-          <div className="govuk-grid-column-one-third">
-                <CardList>
-                  <CardListTitle>About the data service</CardListTitle>
-                  <CardListLink href="/">About the data service</CardListLink>
-                  <CardListLink href="/">IDS Hub</CardListLink>
-                  <CardListLink href="/">Climate change portal</CardListLink>
-                  <CardListLink href="/">Violence against woman and girls portal</CardListLink>
-                </CardList>
-          </div>
+    <footer className="govuk-footer app-footer" role="contentinfo">
+      <div className="app-width-container govuk-grid-row app-footer__container">
+        <div className="govuk-grid-column-two-thirds">
+          <h3 className="govuk-heading-m">Topics</h3>
+          <ul className="govuk-list app-footer__list">
+            {parentTopics.map((item: { title: string; identifier: string }) => (
+              <PopularItem
+                key={item.title}
+                text={item.title}
+                href={item.identifier}
+              />
+            ))}
+          </ul>
         </div>
+        <div className="govuk-grid-column-one-third">
+          <h3 className="govuk-heading-m">About the data service</h3>
+          <ul className="govuk-list app-footer__about-list">
+            {popularItems.map((item) => (
+              <PopularItem key={item.text} text={item.text} href={item.href} />
+            ))}
+          </ul>
+        </div>
+      </div>
 
-        <div className="app-width-container">
-          <div className="govuk-footer__meta">
-            <div className="govuk-footer__meta-item govuk-footer__meta-item--grow">
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                className="govuk-footer__licence-logo"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 483.2 195.7"
-                height="17"
-                width="41"
-              >
-                <path
-                  fill="currentColor"
-                  d="M421.5 142.8V.1l-50.7 32.3v161.1h112.4v-50.7zm-122.3-9.6A47.12 47.12 0 0 1 221 97.8c0-26 21.1-47.1 47.1-47.1 16.7 0 31.4 8.7 39.7 21.8l42.7-27.2A97.63 97.63 0 0 0 268.1 0c-36.5 0-68.3 20.1-85.1 49.7A98 98 0 0 0 97.8 0C43.9 0 0 43.9 0 97.8s43.9 97.8 97.8 97.8c36.5 0 68.3-20.1 85.1-49.7a97.76 97.76 0 0 0 149.6 25.4l19.4 22.2h3v-87.8h-80l24.3 27.5zM97.8 145c-26 0-47.1-21.1-47.1-47.1s21.1-47.1 47.1-47.1 47.2 21 47.2 47S123.8 145 97.8 145"
-                />
-              </svg>
-              <span className="govuk-footer__licence-description">
-                All content is available under the{" "}
-                <a
-                  className="govuk-footer__link"
-                  href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
-                  rel="license"
-                >
-                  Open Government Licence v3.0
-                </a>
-                , except where otherwise stated
-              </span>
-            </div>
-            <div className="govuk-footer__meta-item">
+      <div className="app-width-container">
+        <div className="govuk-footer__meta">
+          <div className="govuk-footer__meta-item govuk-footer__meta-item--grow">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              className="govuk-footer__licence-logo"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 483.2 195.7"
+              height="17"
+              width="41"
+            >
+              <path
+                fill="currentColor"
+                d="M421.5 142.8V.1l-50.7 32.3v161.1h112.4v-50.7zm-122.3-9.6A47.12 47.12 0 0 1 221 97.8c0-26 21.1-47.1 47.1-47.1 16.7 0 31.4 8.7 39.7 21.8l42.7-27.2A97.63 97.63 0 0 0 268.1 0c-36.5 0-68.3 20.1-85.1 49.7A98 98 0 0 0 97.8 0C43.9 0 0 43.9 0 97.8s43.9 97.8 97.8 97.8c36.5 0 68.3-20.1 85.1-49.7a97.76 97.76 0 0 0 149.6 25.4l19.4 22.2h3v-87.8h-80l24.3 27.5zM97.8 145c-26 0-47.1-21.1-47.1-47.1s21.1-47.1 47.1-47.1 47.2 21 47.2 47S123.8 145 97.8 145"
+              />
+            </svg>
+            <span className="govuk-footer__licence-description">
+              All content is available under the{" "}
               <a
-                className="govuk-footer__link govuk-footer__copyright-logo"
-                href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/"
+                className="govuk-footer__link"
+                href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
+                rel="license"
               >
-                © Crown copyright
+                Open Government Licence v3.0
               </a>
-            </div>
-
+              , except where otherwise stated
+            </span>
+          </div>
+          <div className="govuk-footer__meta-item">
+            <a
+              className="govuk-footer__link govuk-footer__copyright-logo"
+              href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/"
+            >
+              © Crown copyright
+            </a>
           </div>
         </div>
+      </div>
     </footer>
   );
 }
