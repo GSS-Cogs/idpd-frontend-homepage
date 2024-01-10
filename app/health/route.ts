462f393c2bf09a5ad9bb0a9d9287bfb928b5605e
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2024-01-10 11:38:17 +0000

    remove comments

--- app/health/route.ts
+++ app/health/route.ts
@@ -123,10 +123,5 @@ const getHeaders = () => {
     "Content-Type": "text/html",
   };
 
-  // const basicAuth = `Basic ${Buffer.from(`${USERNAME}:${PASSWORD}`).toString(
-  //   "base64"
-  // )}`;
-  // headers.Authorization = basicAuth;
-
   return headers;
 };
